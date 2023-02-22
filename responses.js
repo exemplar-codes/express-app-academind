9c2c6b1279b031b2af273491258e5532ab8c6f09
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-23 00:45:17 +0530

    Sending multimedia files, nothing special

--- responses.js
+++ responses.js
@@ -2,7 +2,7 @@ const express = require("express"); // can be named anything, since CommonJS
 
 const app = express();
 
-// HTML response
+// ignore this route and code
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome to the site</h1>
   <h2>Checkout other pages</h2>
@@ -10,10 +10,16 @@ app.get("/", (req, res) => {
     <ul>
       <li><a href="/text">/text</a></li>
       <li><a href="/json">/json</a></li>
+      <li><a href="/html">/html</a></li>
+      <li><a href="/music">/music</a></li>
+      <li><a href="/image">/image</a></li>
     </ul>
   </p>
   `);
 });
+
+// content-type/MIME auto inference
+// for both variables and files, on binary level
 
 app.get("/text", (req, res) => {
   res.send("Hello, world");
@@ -22,5 +28,17 @@ app.get("/text", (req, res) => {
 app.get("/json", (req, res) => {
   res.send({ myMessage: "Hello, world" });
 });
+
+app.get("/html", (req, res) => {
+  res.send("<h1>Hello, world</h1>");
+});
+
+app.get("/image", (req, res) =>
+  res.sendFile("./image.jpg", { root: __dirname })
+);
+
+app.get("/music", (req, res) =>
+  res.sendFile("./perception.mp3", { root: __dirname })
+);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
