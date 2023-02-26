38ade456743a3c186d58cfb2f26225313c73d4a4
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-26 19:50:56 +0530

    Add redirects

--- responses.js
+++ responses.js
@@ -22,6 +22,26 @@ app.get("/", (req, res) => {
   `);
 });
 
+app.get("/here", (req, res) => {
+  res.send(`<h1>Welcome to the site</h1>
+  <h2>Checkout other pages</h2>
+  <p>
+    <ul>
+    <a href="/redirect">Redirect</a>
+      <li><a href="/text">/text</a></li>
+      <li><a href="/json">/json</a></li>
+      <li><a href="/html">/html</a></li>
+      <li><a href="/music">/music</a></li>
+      <li><a href="/image">/image</a></li>
+      <li><a href="/video">/video</a></li>
+      <li><a href="/zip-file">/zip-file</a></li>
+      <br />
+      <li><a href="/music-faked">/music-faked (.mp3 file with .txt extension)</a></li>
+    </ul>
+  </p>
+  `);
+});
+
 // content-type/MIME auto inference
 // for both variables and files, on binary level
 
@@ -30,11 +50,12 @@ app.get("/text", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-  res.send({ myMessage: "Hello, world" });
+  res.json({ myMessage: "Hello, world" });
+  // res.send({ myMessage: "Hello, world" }); // is also fine, but avoid it if possible
 });
 
 app.get("/html", (req, res) => {
-  res.send("<h1>Hello, world</h1>");
+  res.send('<h1>Hello, world </h1><a href="/redirect-back">Redirect</a>');
 });
 
 app.get("/image", (req, res) =>
@@ -56,5 +77,11 @@ app.get("/music-faked", (req, res) =>
 app.get("/zip-file", (req, res) =>
   res.sendFile("./zip-file.zip", { root: __dirname })
 ); // direct consumption not possible, so downloaded
+
+// redirects
+app.get("/redirect", (req, res, next) => {
+  res.redirect("back"); // special meaning, last loaded page, use "/back" if there's an actual route named so
+  // res.redirect("/some-path"); // redirect to /some-path
+});
 
 app.listen(3000, () => console.log("Server running on port 3000"));
